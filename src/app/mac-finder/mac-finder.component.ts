acde6e4d73929fefd81c12d197259728525aee62
Satyam Pandey <128727378+satyamdev88@users.noreply.github.com>
2025-06-22 17:33:48 +0530

    Update mac-finder.component.ts

--- src/app/mac-finder/mac-finder.component.ts
+++ src/app/mac-finder/mac-finder.component.ts
@@ -12,15 +12,20 @@ import { FooterComponent } from '../footer/footer.component';
   styleUrl: './mac-finder.component.scss'
 })
 export class MacFinderComponent {
-mac = '';
+  mac = '';
+  isVisible: boolean = false; // Added through github
   vendor: any;
-
+  // Added through github
+  ngOnInit(): void {
+      this.isVisible = false;
+  }
   constructor(private macService: MacfinderService) {}
 
   find() {
     this.macService.lookup(this.mac.trim()).subscribe({
       next: (data) => {
         this.vendor = data.vendor;
+        this.isVisible = true; // Added through github
       },
       error: (err) => {
         if (err.status === 404) {
@@ -31,6 +36,6 @@ mac = '';
       },
     });
 
-    console.log(this.vendor);
+    // console.log(this.vendor);
   }
 }
